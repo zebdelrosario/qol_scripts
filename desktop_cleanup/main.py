f0248fe95f3bd110804dc7c88fa06600048a8519
zebdelrosario <delrosariozeb@gmail.com>
2022-01-09 23:36:19 +1000

    Fix set_desktop function, add get_desktop_items function

---
+++ desktop_cleanup/main.py
@@ -0,0 +1,50 @@
+"""
+Desktop Cleanup script - A QOL Script
+"""
+import filetype
+import tempfile
+import os
+
+
+def main():
+    """Run the desktop cleaning script."""
+    in_file = "../user_desktop.txt"
+    with open(in_file, 'r') as in_file:
+        if in_file.read() == "":
+            new_desktop_path = input("enter desktop path:\n> ")
+            set_desktop_path(in_file, new_desktop_path)
+        in_file.seek(0)  # Reset position to start
+        desktop_path = str(in_file.read())
+        get_desktop_items(desktop_path)
+
+
+def get_desktop_items(desktop_path):
+    """Return a list of objects from the desktop path."""
+    cwd = os.getcwd()
+    os.chdir(desktop_path)  # Set cwd to desktop
+    desktop_items = [i for i in desktop_path]
+    print(desktop_items)
+
+
+def set_desktop_path(source_path, destination_path):
+    """Save desktop path to .txt file."""
+    temp_file = tempfile.NamedTemporaryFile(mode="r+")
+    # Copy in_file to temporary file
+    with open(source_path, 'r') as in_file:
+        for line in in_file:
+            temp_file.write(line.rstrip() + "\n")
+    temp_file.seek(0)
+    temp_file.write(destination_path)
+    # Write modified contents from temporary file to out_file
+    with open(source_path, 'w') as out_file:
+        for line in temp_file:
+            out_file.write(line)
+    temp_file.close()
+
+
+def navigate_to_desktop(desktop_path):
+    """Get the current working directory."""
+    os.chdir(desktop_path)
+
+
+main()
