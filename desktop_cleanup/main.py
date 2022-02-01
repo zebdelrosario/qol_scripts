b3f5b805f5fb2b22e2cd9abb0c89d01a3f95fbd5
zebdelrosario <delrosariozeb@gmail.com>
2022-02-02 02:00:26 +1000

    Add dictionary to enable file extension comparison, add find_key function

--- desktop_cleanup/main.py
+++ desktop_cleanup/main.py
@@ -1,13 +1,18 @@
 """
 Desktop Cleanup script - A QOL Script
 """
-import filetype
 import tempfile
 import os
 
 
 def main():
     """Run the desktop cleaning script."""
+    file_extension_for_file_type = {"image": ["jpg", "jpeg", "png", "bmp", "gif"],
+                       "video": ["mp4", "avi", "wmv", "mov", "flv", "dv"],
+                       "audio": ["mp3", "aiff", "wav", "wma", "aac", "ra"],
+                       "pdf": ["pdf"],
+                       "microsoft office": ["doc", "docx", "xlsx", "xlsm", "xlsb"]
+                       }
     in_file = "../user_desktop.txt"
     with open(in_file, 'r') as in_file:
         if in_file.read() == "":
@@ -17,7 +22,21 @@ def main():
         desktop_path = str(in_file.read())
         desktop_items = get_desktop_items(desktop_path)
         for desktop_item in desktop_items:
-    ##TODO: Iterate over items and determine their extensions, sort them into relevant directories
+            file_extension = os.path.splitext(desktop_item)
+            key = find_key(file_extension_for_file_type, str(file_extension[1]))
+            if key is not None:
+                print(f'{desktop_item} = {key.upper()}')
+            else:
+                print(f'{desktop_item} = OTHER')
+
+
+def find_key(input_dict, input_value):
+    """Find key of a given dictionary using a value."""
+    for key, value in input_dict.items():
+        if input_value[1:] == value:
+            return key
+        if isinstance(value, list) and input_value[1:] in value:
+            return key
 
 
 def get_desktop_items(desktop_path):
